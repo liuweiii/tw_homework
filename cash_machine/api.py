662f636a9cb6f2805247549e413570bb3a78d74d
liuwei <liuweiiii@163.com>
2016-03-12 21:47:08 +0800

    [tech][liuwei]refactor parse shop list

--- cash_machine/api.py
+++ cash_machine/api.py
@@ -4,6 +4,7 @@ from flask import request, render_template
 from services.cash_machine_service import to_bill_list
 from common.exception import ProductNotFoundException
 from models.discount import Discount
+import json
 
 app = Flask(__name__)
 
@@ -30,29 +31,21 @@ def input_bill():
 
 @app.route("/", methods=["POST"])
 def process_bill():
-    def show_error(message=u"输入参数有误"):
+    def _show_error(message=u"输入参数有误"):
         return render_template("index.html", show="ERROR", message=message)
 
-    def parse_shop_list():
-        temp_data = str(request.form["shop_list"]).strip()
-        if "[" not in temp_data or "]" not in temp_data:
-            return show_error()
-        temp_data = temp_data.strip("[]").split("\r\n")
-        shop_list_ = []
-        for item in temp_data:
-            striped_item = item.strip().strip("',\'")
-            if striped_item not in ("[", "]", ""):
-                shop_list_.append(striped_item)
-        return shop_list_
+    def _parse_shop_list():
+        return json.loads(request.form["shop_list"].replace('\'', '"'))
+
     try:
-        shop_list = parse_shop_list()
+        shop_list = _parse_shop_list()
         bill_list = to_bill_list(shop_list)
         out, total, save = render_out(bill_list)
         return render_template("index.html", show="RESULT", out=out, total=total, save=save,
                                discount_sum=Discount.summarize())
     except ValueError:
-        return show_error()
+        return _show_error()
     except ProductNotFoundException:
-        return show_error(u"输入的条形码找不到对应商品")
+        return _show_error(u"输入的条形码找不到对应商品")
     except Exception as e:
-        return show_error(u"这个错误[{0}]没被考虑到... ...".format(e.message))
+        return _show_error(u"这个错误[{0}]没被考虑到... ...".format(e.message))
